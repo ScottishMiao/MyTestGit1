4687842bf01f91feafd24459d230e4802f74acb5
ScottishMiao <oversea4415@naver.com>
2016-06-25 15:57:46 +0900

    report card: Add Comments

--- report_card.c
+++ report_card.c
@@ -2,9 +2,11 @@
 
 int main()
 {
+	// 변수
 	int kor, eng, math;
 	int sum = 0;
 
+	// 값 입력
 	math = 80;
 	eng = 100;
 	kor = 90;
@@ -12,10 +14,12 @@ int main()
 
 	printf("This program print report card.\n");
 
+	// 성적 추가
 	printf("Korean : %d\n", kor);
 	printf("English : %d\n", eng);
 	printf("Math : %d\n", math);
 	printf("Sum : %d\n", sum);
 	printf("Average : %d\n", sum/3);
+	
 	return 0;
 }
